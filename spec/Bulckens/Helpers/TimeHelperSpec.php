7b2510ca7945427c742e182fc4a647ec4b87dcfd
wout <wout@impinc.co.uk>
2017-02-19 20:39:20 +0100

    Time from strings

--- spec/Bulckens/Helpers/TimeHelperSpec.php
+++ spec/Bulckens/Helpers/TimeHelperSpec.php
@@ -2,6 +2,7 @@
 
 namespace spec\Bulckens\Helpers;
 
+use stdClass;
 use Bulckens\Helpers\TimeHelper;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
@@ -13,9 +14,107 @@ class TimeHelperSpec extends ObjectBehavior {
     $this::milliseconds()->shouldBeDouble();
   }
 
+  
   // Ms method method
   function it_returns_current_milliseconds_as_alias() {
     $this::ms()->shouldBeDouble();
+  }
+
+  
+  // Sec method
+  function it_parses_a_given_string_representation_to_time_in_milliseconds() {
+    $this::sec( '523ms' )->shouldBe( 0.523 );
+  }
+
+  function it_parses_a_given_string_representation_to_time_in_seconds() {
+    $this::sec( '10s' )->shouldBe( 10 );
+  }
+
+  function it_parses_a_given_string_representation_to_time_in_minutes() {
+    $this::sec( '5m' )->shouldBe( 5 * 60 );
+  }
+
+  function it_parses_a_given_string_representation_to_time_in_hours() {
+    $this::sec( '7h' )->shouldBe( 7* 60 * 60 );
+  }
+
+  function it_parses_a_given_string_representation_to_time_in_days() {
+    $this::sec( '2d' )->shouldBe( 2 * 24 * 60 * 60 );
+  }
+
+  function it_parses_a_given_string_representation_to_time_in_months() {
+    $this::sec( '2M' )->shouldBe( intval( 365 / 12 * 2 * 24 * 60 * 60 ) );
+  }
+
+  function it_parses_a_given_string_representation_to_time_in_zero_months() {
+    $this::sec( '0M' )->shouldBe( 0 );
+  }
+
+  function it_parses_a_given_string_representation_to_time_in_years() {
+    $this::sec( '2y' )->shouldBe( 365 * 2 * 24 * 60 * 60 );
+  }
+
+  function it_parses_a_given_string_representation_to_time_in_zero_years() {
+    $this::sec( '0y' )->shouldBe( 0 );
+  }
+
+  function it_accepts_a_given_number_as_seconds() {
+    $this::sec( 15 )->shouldBe( 15 );
+  }
+
+  function it_fails_when_given_anything_other_than_a_string_or_number_for_from() {
+    $this::shouldThrow( 'Bulckens\Helpers\TimeNotConvertableException' )->duringIs( new stdClass() );
+  }
+
+
+  // Is method
+  function it_tests_true_if_a_given_string_represents_time_in_milliseconds() {
+    $this::is( '523ms' )->shouldBe( true );
+  }
+
+  function it_tests_true_if_a_given_string_represents_time_in_seconds() {
+    $this::is( '10s' )->shouldBe( true );
+  }
+
+  function it_tests_true_if_a_given_string_represents_time_in_minutes() {
+    $this::is( '5m' )->shouldBe( true );
+  }
+
+  function it_tests_true_if_a_given_string_represents_time_in_hours() {
+    $this::is( '7h' )->shouldBe( true );
+  }
+
+  function it_tests_true_if_a_given_string_represents_time_in_days() {
+    $this::is( '2d' )->shouldBe( true );
+  }
+
+  function it_tests_true_if_a_given_string_represents_time_in_months() {
+    $this::is( '2M' )->shouldBe( true );
+  }
+
+  function it_tests_true_if_a_given_string_represents_time_in_years() {
+    $this::is( '2y' )->shouldBe( true );
+  }
+
+  function it_tests_true_for_a_given_string_number_as_seconds() {
+    $this::is( '531' )->shouldBe( true );
+  }
+
+  function it_tests_true_for_a_given_number_as_seconds() {
+    $this::is( 36 )->shouldBe( true );
+  }
+
+  function it_fails_when_given_anything_other_than_a_string_or_number_for_is() {
+    $this::shouldThrow( 'Bulckens\Helpers\TimeNotConvertableException' )->duringIs( new stdClass() );
+  }
+
+
+  // Parse method
+  function it_parses_time_to_two_parts() {
+    $parts = $this::parse( '36d' );
+    $parts->shouldBeArray();
+    $parts[1]->shouldBe( '36' );
+    $parts[2]->shouldBe( 'd' );
   }
 
 }
--- src/Bulckens/Helpers/TimeHelper.php
+++ src/Bulckens/Helpers/TimeHelper.php
@@ -2,6 +2,8 @@
 
 namespace Bulckens\Helpers;
 
+use Exception;
+
 class TimeHelper {
 
   // Get time in milliseconds
@@ -9,9 +11,71 @@ class TimeHelper {
     return round( microtime( 1 ) * 1000 );
   }
 
+
   // Get time in milliseconds (alias)
   public static function ms() {
     return self::milliseconds();
   }
+
+
+  // Parse time string to parts
+  public static function parse( $time ) {
+    if ( preg_match( '/\A([0-9]+)(ms|s|m|h|d|M|y)\z/', $time, $match ) )
+      return $match;
+  }
+
+
+  // Parse time string to seconds
+  public static function sec( $time ) {
+    // if numeric, return as is
+    if ( is_numeric( $time ) )
+      return $time * 1;
+
+    // parse string values
+    if ( self::is( $time ) ) {
+      $parts = self::parse( $time );
+
+      switch ( $parts[2] ) {
+        case 'ms':
+          return $parts[1] / 1000;
+        break;
+        case 's':
+          return $parts[1] * 1;
+        break;
+        case 'm':
+          return $parts[1] * 60;
+        break;
+        case 'h':
+          return $parts[1] * 60 * 60;
+        break;
+        case 'd':
+          return $parts[1] * 60 * 60 * 24;
+        break;
+        case 'M':
+          return intval( 365 / 12 * $parts[1] * 24 * 60 * 60 );
+        break;
+        case 'y':
+          return $parts[1] * 365 * 24 * 60 * 60;
+        break;
+      }
+    }
+  }
+
+
+  // Test given string as time
+  public static function is( $time ) {
+    // if numeric, true
+    if ( is_numeric( $time ) ) return true;
+
+    // if string, try to parse it
+    if ( is_string( $time ) )
+      return !! self::parse( $time );
+
+    throw new TimeNotConvertableException( 'Expected string or number but got ' . gettype( $time ) );
+  }
   
 }
+
+
+// Exceptions
+class TimeNotConvertableException extends Exception {}
