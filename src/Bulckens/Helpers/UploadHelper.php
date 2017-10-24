442fd331239266d90aeec2c6c22f84ae15976b00
wout <wout@mick-wout.com>
2017-10-24 20:35:55 +0200

    Upload helper to reoraginize PHP $_FILES upload array

--- src/Bulckens/Helpers/UploadHelper.php
+++ src/Bulckens/Helpers/UploadHelper.php
@@ -7,11 +7,18 @@ class UploadHelper {
   // Build a proper array from uploaded files
   public static function files() {
     global $_FILES;
+    $array = ArrayHelper::flattenDelimited( $_FILES );
     $files = [];
 
-    
+    foreach ( $array as $key => $value ) {
+      if ( preg_match( "/^([^.]+)\.(name|error|size|tmp_name|type)\.(.*)$/", $key, $m ) ) {
+        $key = sprintf( '%s.%s.%s', $m[1], $m[3], $m[2] );
+      }
 
-    return $files;
+      $files[$key] = $value;
+    }
+
+    return ArrayHelper::expandDelimited( $files );
   }
 
 }
